55552dc4a4276f08db9975b5507396eb3f354222
Paola Alonzo <70167540+PaoAlonzo@users.noreply.github.com>
2024-10-01 22:43:32 -0600

    update dashboard

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,217 +1,85 @@
-import React, { useState,useEffect } from 'react';
-import { useNavigate } from 'react-router-dom'; // Importar useNavigate para redirección
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import './Dashboard.css';
-
-import { Chart, ArcElement, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend ,RadialLinearScale } from 'chart.js';
-import { Bar, Doughnut, PolarArea,Pie  } from 'react-chartjs-2'; // Para las estadísticas
+import { Bar, Doughnut, PolarArea, Pie } from 'react-chartjs-2';
 import ClimateVisualization from './ClimateVisualization';
 
-Chart.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
-Chart.register(ArcElement, Title, Tooltip, Legend);
-Chart.register(ArcElement, Tooltip, Legend,RadialLinearScale );
-Chart.register(ArcElement, Tooltip, Legend);
-
-
+// Dummy data for users
+const users = [
+    {
+        id: 1,
+        name: 'John Doe',
+        balance: 100,
+        status: 'Dentro',
+        lastEntry: '2024-09-20 08:00',
+        lastExit: '2024-09-20 17:00',
+        role: 'Usuario',
+        rfid: 'ABC123',
+        history: [
+            { entry: '2024-09-20 08:00', exit: '2024-09-20 17:00' },
+            { entry: '2024-09-19 09:00', exit: '2024-09-19 18:00' },
+        ],
+    },
+    {
+        id: 2,
+        name: 'Jane Smith',
+        balance: 50,
+        status: 'Fuera',
+        lastEntry: '2024-09-19 09:30',
+        lastExit: '2024-09-19 18:15',
+        role: 'Administrador',
+        rfid: 'XYZ789',
+        history: [
+            { entry: '2024-09-19 09:30', exit: '2024-09-19 18:15' },
+            { entry: '2024-09-18 08:30', exit: '2024-09-18 17:45' },
+        ],
+    },
+];
 
 const Dashboard = () => {
-    const navigate = useNavigate(); // Inicializar useNavigate
+    const navigate = useNavigate();
     const [activePanel, setActivePanel] = useState('controlPanel');
+    const [selectedUser, setSelectedUser] = useState(null);
+    const [balanceChange, setBalanceChange] = useState(0);
 
-    // Manejar la salida
     const handleLogout = () => {
-        navigate('/'); // Suponiendo que la ruta '/' es la página de inicio de sesión
+        navigate('/');
     };
 
-    // Datos de la gráfica
-    const data = {
-        labels: ['Espacios Ocupados', 'Espacios Disponibles'],
-        datasets: [
-            {
-                label: 'Estadísticas del Parqueo',
-                data: [20, 90], // Ejemplo: 10 ocupados y 90 disponibles
-                backgroundColor: ['rgba(153, 102, 255, 0.6)', '#9BD0F5'],
-            },
-        ],
+    // Function to handle selecting a user
+    const handleUserClick = (user) => {
+        setSelectedUser(user);
     };
 
-    const options = {
-        responsive: true,
-        plugins: {
-            legend: {
-                position: 'top',
-            },
-            title: {
-                display: true,
-                text: 'Monitoreo de Parqueo',
-            },
-        },
+    // Function to handle balance change
+    const handleBalanceChange = (e) => {
+        setBalanceChange(Number(e.target.value));
     };
 
-    const DataAnillo = () => {
-        const [data, setData] = useState({
-            labels: ['Espacios Ocupados', 'Espacios Disponibles'],
-            datasets: [
-                {
-                    label: 'Monitoreo de Parqueo',
-                    data: [10, 90],
-                    backgroundColor: ['#36a2eb', '#ffce56'],
-                    borderWidth: 1,
-                },
-            ],
-        });
-    
-        useEffect(() => {
-            const interval = setInterval(() => {
-                const newOccupied = Math.floor(Math.random() * 100);
-                const newAvailable = 100 - newOccupied;
-    
-                setData({
-                    labels: ['Espacios Ocupados', 'Espacios Disponibles'],
-                    datasets: [
-                        {
-                            label: 'Monitoreo de Parqueo',
-                            data: [newOccupied, newAvailable],
-                            backgroundColor: ['#36a2eb', '#ffce56'],
-                        },
-                    ],
-                });
-            }, 1000);
-    
-            return () => clearInterval(interval);
-        }, []);
-    
-        return <Doughnut data={data} />;
+    const updateBalance = () => {
+        if (selectedUser) {
+            selectedUser.balance += balanceChange;
+            setSelectedUser({ ...selectedUser });
+            setBalanceChange(0); // Reset the input after updating
+        }
     };
-    
-    /*const dataAnillo = {
-        labels: ['Espacios Ocupados', 'Espacios Disponibles'],
-        datasets: [
-            {
-                label: 'Monitoreo de Parqueo',
-                data: [10, 90], // Ejemplo: 10 ocupados y 90 disponibles
-                backgroundColor: ['#36a2eb', '#ffce56'],
-            },
-        ],
-    };*/
-
-    /*const optionsAnillo = {
-        responsive: true,
-        plugins: {
-            legend: {
-                position: 'top',
-            },
-            title: {
-                display: true,
-                text: 'Estadísticas del Parqueo',
-            },
-        },
-    };*/
-
-    const dataPolarArea = {
-        labels: ['Rojo', 'Azul', 'Amarillo', 'Verde', 'Morado', 'Naranja'],
-        datasets: [
-            {
-                label: 'Cantidad',
-                data: [11, 16, 7, 3, 14, 6],
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.6)',
-                    'rgba(54, 162, 235, 0.6)',
-                    'rgba(255, 206, 86, 0.6)',
-                    'rgba(75, 192, 192, 0.6)',
-                    'rgba(153, 102, 255, 0.6)',
-                    'rgba(255, 159, 64, 0.6)',
-                ],
-                borderColor: [
-                    'rgba(255, 99, 132, 1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)',
-                ],
-                borderWidth: 1,
-            },
-        ],
-    };
-
-    const optionsPolarArea = {
-        responsive: true,
-        plugins: {
-            legend: {
-                position: 'top',
-            },
-            title: {
-                display: true,
-                text: 'Gráfico de Área Polar',
-            },
-        },
-    };
-
-    const dataPie = {
-        labels: ['Rojo', 'Azul', 'Amarillo', 'Verde'],
-        datasets: [
-            {
-                label: 'Cantidad',
-                data: [12, 19, 3, 5],
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.6)',
-                    'rgba(54, 162, 235, 0.6)',
-                    'rgba(255, 206, 86, 0.6)',
-                    'rgba(75, 192, 192, 0.6)',
-                ],
-                borderColor: [
-                    'rgba(255, 99, 132, 1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                ],
-                borderWidth: 1,
-            },
-        ],
-    };
-    
-    const optionsPie = {
-        responsive: true,
-        plugins: {
-            legend: {
-                position: 'top',
-            },
-            title: {
-                display: true,
-                text: 'Gráfico de Pie',
-            },
-        },
-    }
 
     return (
         <div className="dashboard">
-            {/* Sidebar */}
             <aside className="sidebar">
                 <div className="logo">👦🏼 Admin</div>
                 <nav>
                     <ul>
-                        <li
-                            className={activePanel === 'controlPanel' ? 'active' : ''}
-                            onClick={() => setActivePanel('controlPanel')}
-                        >
+                        <li className={activePanel === 'controlPanel' ? 'active' : ''} onClick={() => setActivePanel('controlPanel')}>
                             <span>📊</span> Panel de Control
                         </li>
-                        <li
-                            className={activePanel === 'climatePanel' ? 'active' : ''}
-                            onClick={() => setActivePanel('climatePanel')}
-                        >
+                        <li className={activePanel === 'climatePanel' ? 'active' : ''} onClick={() => setActivePanel('climatePanel')}>
                             <span>🌡️</span> Panel de Clima
                         </li>
-                        <li
-                            className={activePanel === 'usersPanel' ? 'active' : ''}
-                            onClick={() => setActivePanel('usersPanel')}
-                        >
+                        <li className={activePanel === 'usersPanel' ? 'active' : ''} onClick={() => setActivePanel('usersPanel')}>
                             <span>👥</span> Panel de Usuarios
                         </li>
-                        <li
-                            className={activePanel === 'parkingMonitoringPanel' ? 'active' : ''}
-                            onClick={() => setActivePanel('parkingMonitoringPanel')}
-                        >
+                        <li className={activePanel === 'parkingMonitoringPanel' ? 'active' : ''} onClick={() => setActivePanel('parkingMonitoringPanel')}>
                             <span>🚗</span> Panel de Monitoreo de Parqueo
                         </li>
                     </ul>
@@ -219,94 +87,82 @@ const Dashboard = () => {
                 <button className="logout" onClick={handleLogout}>Cerrar sesión</button>
             </aside>
 
-            {/* Main Content */}
             <div className="main-content">
                 <header className="header">
                     <input type="text" className="search" placeholder="Buscar" />
                     <div className="user">
                         <span>Admin</span>
-                        <img
-                            src="https://via.placeholder.com/40"
-                            alt="User Profile"
-                            className="user-avatar"
-                        />
+                        <img src="https://via.placeholder.com/40" alt="User Profile" className="user-avatar" />
                     </div>
                 </header>
 
-                {/* Conditional Content for Each Panel */}
                 <div className="panel-content">
-                    {activePanel === 'controlPanel' && (
-                        <div>
-                            <h2>Panel de Control</h2>
-                            <p>Bienvenido al Panel de Control. Aquí puedes navegar por los diferentes paneles.</p>
-
-                            {/* Previsualización de los otros paneles */}
-                            <div className="preview-section">
-                                <h3>Previsualización de Paneles</h3>
-                                <div className="cards-section">
-                                    <div className="card">
-                                        <h4>Panel de Clima</h4>
-                                        <p>Temperatura: 25°C</p>
-                                        <p>Humedad: 70%</p>
-                                    </div>
-                                    <div className="card">
-                                        <h4>Panel de Usuarios</h4>
-                                        <p>Usuarios registrados: 150</p>
-                                    </div>
-                                    <div className="card">
-                                        <h4>Monitoreo de Parqueo</h4>
-                                        <p>Vehículos dentro: 10</p>
-                                        <p>Espacios disponibles: 90</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    )}
-
-
-
-                    {activePanel === 'climatePanel' && (
-                        <div>
-                            <h2>Panel de Clima</h2>
-                            
-                            <ClimateVisualization />
-                            
-                        </div>
-                    )}
-
                     {activePanel === 'usersPanel' && (
                         <div>
                             <h2>Panel de Usuarios</h2>
                             <p>Listado de todos los usuarios registrados en el sistema.</p>
-                            {/* Aquí podrías mostrar la tabla de usuarios y sus detalles */}
+
+                            <div className="panelcardParqueo">
+                                {users.map((user) => (
+                                    <div className="cardParqueo" key={user.id} onClick={() => handleUserClick(user)}>
+                                        <h3>{user.name}</h3>
+                                        <p>Saldo Disponible: {user.balance}</p>
+                                        <p>Estado: {user.status}</p>
+                                        <p>Último Ingreso: {user.lastEntry}</p>
+                                        <p>Último Egreso: {user.lastExit}</p>
+                                        <p>Rol: {user.role}</p>
+                                    </div>
+                                ))}
+                            </div>
+
+                            {selectedUser && (
+                                <div className="modal">
+                                    <div className="modal-content">
+                                        <h3>Detalles del Usuario</h3>
+                                        <p>Nombre: {selectedUser.name}</p>
+                                        <p>Saldo Disponible: {selectedUser.balance}</p>
+                                        <p>RFID: {selectedUser.rfid}</p>
+                                        <h4>Historial de Ingresos y Egresos</h4>
+                                        <ul>
+                                            {selectedUser.history.map((record, index) => (
+                                                <li key={index}>Ingreso: {record.entry}, Egreso: {record.exit}</li>
+                                            ))}
+                                        </ul>
+
+                                        <h4>Modificar Saldo</h4>
+                                        <input
+                                            type="number"
+                                            value={balanceChange}
+                                            onChange={handleBalanceChange}
+                                            placeholder="Añadir/Restar saldo"
+                                        />
+                                        <button onClick={updateBalance}>Actualizar Saldo</button>
+                                        <button onClick={() => setSelectedUser(null)}>Cerrar</button>
+                                    </div>
+                                </div>
+                            )}
                         </div>
                     )}
+
+                    {/* Other Panels (Control, Climate, Parking Monitoring) */}
+                    {activePanel === 'controlPanel' && (
+                        <div>
+                            <h2>Panel de Control</h2>
+                            <p>Bienvenido al Panel de Control. Aquí puedes navegar por los diferentes paneles.</p>
+                        </div>
+                    )}
+
+                    {activePanel === 'climatePanel' && (
+                        <div>
+                            <h2>Panel de Clima</h2>
+                            <ClimateVisualization />
+                        </div>
+                    )}
+
                     {activePanel === 'parkingMonitoringPanel' && (
                         <div>
                             <h2>Panel de Monitoreo del Parqueo</h2>
                             <p>Estadísticas en tiempo real del parqueo.</p>
-                            <div className='panelcardParqueo'>
-                                {/* Aquí podrías mostrar las estadísticas del parqueo */}
-                                <div className="cardParqueo">
-                                    <h2>Panel de Monitoreo del Parqueo</h2>
-                                    <Bar data={data} options={options} />
-                                </div>
-
-                                <div className="cardParqueo">
-                                    <h2>Panel de Monitoreo del Parqueo</h2>
-                                    <DataAnillo />
-                                </div>
-
-                                <div className="cardParqueo">
-                                    <h2>Panel de Monitoreo del Parqueo</h2>
-                                    <PolarArea data={dataPolarArea} options={optionsPolarArea} />
-                                </div>
-
-                                <div className="cardParqueo">
-                                    <h2>Panel de Monitoreo del Parqueo</h2>
-                                    <Pie data={dataPie} options={optionsPie} />
-                                </div>
-                            </div>
                         </div>
                     )}
                 </div>
